e159da286f78922433e31c51a65ec9274dd3d57c
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-10 00:18:46 +0200

    Added GetProductByCategory Handler and Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/categories/{category}", async (string category, ISender sender) =>
+        {
+            var query = new GetProductByCategoryQuery(category);
+
+            var result = await sender.Send(query);
+
+            var productResult = result.Adapt<GetProductByCategoryResponse>();
+
+            return Results.Ok(productResult);
+
+        }).WithName("GetProductByCategory")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status500InternalServerError)
+        .WithDescription("Get Product By Category")
+        .WithSummary("Get Product By Category");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryQueryResult>;
+public record GetProductByCategoryQueryResult(IEnumerable<Product> Products);
+
+internal class GetProductByCategoryQueryHandler
+    (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryQueryResult>
+{
+    public async Task<GetProductByCategoryQueryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
+
+        var products = await session.Query<Product>()
+            .Where(p => p.Categories.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+
+        return new GetProductByCategoryQueryResult(products);
+    }
+}
