840484c9e17a90d7d61279d386633e00db23ba1f
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-09 23:12:15 +0200

    Implemented GetProductById Endpoint and Handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdResult(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var query = new GetProductByIdQuery(id);
+
+            var result = await sender.Send(query);
+
+            var product = result.Adapt<GetProductByIdResult>();
+
+            return Results.Ok(product);
+
+        }).WithName("GetProductById")
+        .Produces<GetProductByIdResult>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status500InternalServerError)
+        .WithDescription("Get Product By Id")
+        .WithSummary("Get Product By Id");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdQueryResponse>;
+public record GetProductByIdQueryResponse(Product Product);
+
+internal class GetProductByIdQueryHandler
+    (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdQueryResponse>
+{
+    public async Task<GetProductByIdQueryResponse> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"{nameof(GetProductByIdQueryHandler)}.Handle is being executed with {{@Query}}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        return product == null ? throw new ProductNotFoundException() : new GetProductByIdQueryResponse(product);
+    }
+}
