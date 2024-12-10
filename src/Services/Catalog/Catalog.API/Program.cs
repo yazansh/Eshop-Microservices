490a92cd26f8ce5ececc28c8bec0b5e78dde80ef
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-10 19:01:42 +0200

    Supported LogginBehavior in MediatR

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -5,6 +5,7 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 builder.Services.AddValidatorsFromAssembly(assembly);
 
