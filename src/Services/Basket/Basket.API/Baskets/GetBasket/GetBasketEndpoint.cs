2e85b9e70b90f421cd636aa99a916891a377bcbf
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-12 16:38:03 +0200

    Defined the GetBasket Endpoint and Handler with temp implmenetation

---
+++ src/Services/Basket/Basket.API/Baskets/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.API.Baskets.GetBasket;
+
+public record GetBasketResponse(ShoppingCart Cart);
+
+public class GetBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/baskets/{username}", async (string username, ISender sender) =>
+        {
+            var result = await sender.Send(new GetBasketQuery(username));
+
+            var response = result.Adapt<GetBasketResponse>();
+
+            return Results.Ok(response);
+        }).WithName("GetBasket")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status500InternalServerError)
+        .WithDescription("Get Basket By Username")
+        .WithSummary("Get Basket By Username");
+    }
+}
---
+++ src/Services/Basket/Basket.API/Baskets/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Baskets.GetBasket;
+
+public record GetBasketQuery(string Username) : IQuery<GetBasketQueryResult>;
+public record GetBasketQueryResult(ShoppingCart Cart);
+
+public class GetBasketHandler
+    (IDocumentSession session)
+    : IQueryHandler<GetBasketQuery, GetBasketQueryResult>
+{
+    public async Task<GetBasketQueryResult> Handle(GetBasketQuery request, CancellationToken cancellationToken)
+    {
+        //// TODO: Create an abstraction using repository pattern to have DB Repo and Redis repo
+        //var basket = await session.Query<ShoppingCart>()
+        //    .Where(sc => sc.Username.Equals(request.Username))
+        //    .FirstOrDefaultAsync(cancellationToken);
+
+        //return  basket is null ? throw new BasketNotFoundException(request.Username) : new GetBasketQueryResult(basket);
+
+        return new GetBasketQueryResult(new ShoppingCart("test"));
+    }
+}
