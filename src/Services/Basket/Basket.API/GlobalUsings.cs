54e72c35c6e565b52958e419e6db14d42fbc1249
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-12 16:38:22 +0200

    Registered services and added required middlewares in the Program.cs

--- src/Services/Basket/Basket.API/GlobalUsings.cs
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -1,5 +1,10 @@
 ﻿global using Marten;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
 global using Basket.API.Models;
 global using Basket.API.Exceptions;
 global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions;
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddCarter(new DependencyContextAssemblyCatalog(assemblies: assembly));
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
+
+app.MapCarter();
+
 
 app.Run();
