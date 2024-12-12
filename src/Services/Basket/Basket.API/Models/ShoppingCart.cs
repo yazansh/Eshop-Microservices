783db1924743dcebb9f174b8246ae0166a035c45
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-12 16:03:58 +0200

    Defined models

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public required string Username { get; set; }
+
+    public List<ShoppingCartItem> Items { get; set; } = [];
+
+    public double TotalPrice => Items.Sum(i => i.Price * i.Quantity);
+
+    [SetsRequiredMembers]
+    public ShoppingCart(string userName)
+    {
+        Username = userName;
+    }
+
+    public ShoppingCart()
+    {
+    }
+}
---
+++ src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCartItem
+{
+    public string? Color { get; set; }
+
+    public double Price { get; set; }
+
+    public int Quantity { get; set; }
+
+    public Guid ProductId { get; set; }
+
+    public string ProductName { get; set; } = string.Empty;
+}
