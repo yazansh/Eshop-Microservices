68e314fa3cb0407b47844a5247aac7695d36ab39
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-12 17:22:35 +0200

    Added DeleteBasket Endpoint and Handler

---
+++ src/Services/Basket/Basket.API/Baskets/DeleteBasket/DeleteBasketEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.API.Baskets.DeleteBasket;
+
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/baskets/{username}", async (string username, ISender sender) =>
+        {
+            var command = new DeleteBasketCommand(username);
+
+            var result = await sender.Send(command);
+
+            return Results.Ok(result.Adapt<DeleteBasketResponse>());
+        })
+        .WithName("DeleteBasket")
+        .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status500InternalServerError)
+        .WithDescription("Delete Basket")
+        .WithSummary("Delete Basket");
+    }
+}
---
+++ src/Services/Basket/Basket.API/Baskets/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Baskets.DeleteBasket;
+
+public record DeleteBasketCommand(string Username) : ICommand<DeleteBasketCommandResult>;
+public record DeleteBasketCommandResult(bool IsSuccess);
+
+// TODO: add command validation 
+
+internal class DeleteBasketHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketCommandResult>
+{
+    public Task<DeleteBasketCommandResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+    {
+        // TODO: call the repository delete method
+
+        return Task.FromResult(new DeleteBasketCommandResult(true));
+    }
+}
