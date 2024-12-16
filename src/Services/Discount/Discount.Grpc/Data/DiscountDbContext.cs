a1001b75151ac59cef68e7b2208374481efd8950
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-16 14:36:40 +0200

    Installed EF Core Sqllite and EF Core Tools. Created Coupon Model and DbContext

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public class DiscountDbContext : DbContext
+{
+    public DbSet<Coupon> Coupons { get; set; }
+
+    public DiscountDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
