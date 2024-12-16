f932d0a4442fd61a4741c93694bd58c2c85820b3
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-16 14:40:31 +0200

    defined discount.proto endpoints and DiscountServices using DbContext

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,78 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService
+    (DiscountDbContext dbContext)
+    : Discount.DiscountBase
+{
+
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = new Coupon()
+        {
+            ProductName = request.Coupon.ProductName,
+            Description = request.Coupon.Description,
+            Amount = request.Coupon.Amount,
+        };
+
+        await dbContext.Coupons.AddAsync(coupon);
+        await dbContext.SaveChangesAsync();
+
+        return new CouponModel
+        {
+            Id = coupon.Id,
+            ProductName = coupon.ProductName,
+            Description = coupon.Description,
+            Amount = coupon.Amount,
+        };
+    }
+
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext.Coupons
+            .Where(c => c.ProductName.Equals(request.ProductName))
+            .FirstOrDefaultAsync() ?? throw new Exception($"Product name: {request.ProductName} was not found!");
+
+        return new CouponModel
+        {
+            Id = coupon.Id,
+            ProductName = coupon.ProductName,
+            Description = coupon.Description,
+            Amount = coupon.Amount,
+        };
+    }
+
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext.Coupons
+            .Where(c => c.ProductName.Equals(request.Coupon.ProductName))
+            .FirstOrDefaultAsync() ?? throw new Exception($"Product name: {request.Coupon.ProductName} was not found!");
+
+        dbContext.Coupons.Update(coupon);
+        await dbContext.SaveChangesAsync();
+
+        return new CouponModel
+        {
+            Id = coupon.Id,
+            ProductName = coupon.ProductName,
+            Description = coupon.Description,
+            Amount = coupon.Amount,
+        };
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext.Coupons
+            .Where(c => c.ProductName.Equals(request.ProductName))
+            .FirstOrDefaultAsync() ?? throw new Exception($"Product name: {request.ProductName} was not found!");
+
+        dbContext.Coupons.Remove(coupon);
+        await dbContext.SaveChangesAsync();
+
+        return new DeleteDiscountResponse { Success = true };
+    }
+}
