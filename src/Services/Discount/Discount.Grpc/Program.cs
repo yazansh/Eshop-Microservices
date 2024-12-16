3166575bf8a36c3ca2efd9bb1191d1c14d208a6a
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-16 14:42:53 +0200

    registered dbcontext and grpc services in program.cs and added db connection string in appsettings

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,20 @@
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DiscountDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<DiscountService>();
+
+app.Run();
