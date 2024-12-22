93c99a57043ca2c1d179479cc105b3868ddd9455
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-22 20:09:57 +0200

    Created Migrations

--- src/Services/Ordering/Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Infrastructure/GlobalUsing.cs
@@ -1,4 +1,6 @@
 ﻿global using Ordering.Domain.Models;
 global using Ordering.Domain.Enums;
 global using Ordering.Domain.ValueObjects;
+global using Ordering.Infrastructure.Data;
+global using Ordering.Infrastructure.Extensions;
 global using Microsoft.EntityFrameworkCore;
