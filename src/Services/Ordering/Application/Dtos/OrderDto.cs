2521a64239f727b5e0ca8c40f470c12d26344b97
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-23 14:48:57 +0200

    Created Application DTOs

---
+++ src/Services/Ordering/Application/Dtos/OrderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Dtos;
+public record OrderDto(
+    Guid Id,
+    string OrderName,
+    Guid CustomerId,
+    AddressDto BillingAddress,
+    AddressDto ShippingAddress,
+    PaymentDto Payment,
+    List<OrderItemDto> OrderItems);
---
+++ src/Services/Ordering/Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Ordering.Application.Dtos;
+public record OrderItemDto(
+    Guid ProductId,
+    decimal Price,
+    int Quantity);
