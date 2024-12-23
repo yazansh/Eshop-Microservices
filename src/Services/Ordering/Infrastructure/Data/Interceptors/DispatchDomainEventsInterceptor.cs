0167c2d236d21e1e8f2ee0e1eb2bc8e3e9920594
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-23 14:22:15 +0200

    Implemented DispatchDomainEventsInterceptor and published events using MediatR publish

---
+++ src/Services/Ordering/Infrastructure/Data/Interceptors/DispatchDomainEventsInterceptor.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Ordering.Domain.Abstractions;
+
+namespace Ordering.Infrastructure.Data.Interceptors;
+public class DispatchDomainEventsInterceptor(IMediator mediator) : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        DispatchDomanEventsAsync(eventData.Context).GetAwaiter().GetResult();
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        await DispatchDomanEventsAsync(eventData.Context);
+        return await base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private async Task DispatchDomanEventsAsync(DbContext? context)
+    {
+        if (context == null) return;
+
+        var aggregates = context.ChangeTracker
+            .Entries<IAggregate>()
+            .Where(ee => ee.Entity.DomainEvents.Any())
+            .Select(ee => ee.Entity);
+
+        var domainEvents = aggregates.SelectMany(ee => ee.DomainEvents).ToList();
+
+        aggregates.ToList().ForEach(a => a.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+            await mediator.Publish(domainEvent);
+    }
+}
