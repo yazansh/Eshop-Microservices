ff020b2bbe150911d8b3d791a3a4fe06d51d6781
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-23 15:39:16 +0200

    Implemented DeleteOrder Command and handler

---
+++ src/Services/Ordering/Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+public record DeleteOrderCommand(Guid Id) : ICommand<DeleteOrderCommandResult>;
+public record DeleteOrderCommandResult(bool IsSuccess);
+
+public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteOrderCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotNull().WithMessage("Id is required")
+            .NotEqual(Guid.Empty).WithMessage("Id is required");
+    }
+}
---
+++ src/Services/Ordering/Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+public class DeleteOrderCommandHandler
+    (IApplicationDbContext dbContext)
+    : ICommandHandler<DeleteOrderCommand, DeleteOrderCommandResult>
+{
+    public async Task<DeleteOrderCommandResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        var order = await dbContext.Orders.FindAsync([OrderId.Of(command.Id)], cancellationToken)
+            ?? throw new OrderNotFoundException(command.Id);
+
+        dbContext.Orders.Remove(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteOrderCommandResult(true);
+    }
+}
