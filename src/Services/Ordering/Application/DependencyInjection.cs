d040e1d18063b0ce763876d4cf0eff8ca0004c5f
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-24 17:06:12 +0200

    registered DIs and the custom exception handler and the health check for the Order API project

---
+++ src/Services/Ordering/Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Ordering.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configs =>
+        {
+            configs.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            configs.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            configs.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        return services;
+    }
+}
--- src/Services/Ordering/Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Infrastructure/DependencyInjection.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Ordering.Infrastructure;
 public static class DependencyInjection
 {
-    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
         services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
@@ -19,6 +19,8 @@ public static class DependencyInjection
         );
 
         services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+        return services;
     }
 
     public static async Task InitialiseDatabaseAsync(this WebApplication app)
