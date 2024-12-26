f432903f17fa2cf7b4cdb8b2588f23527537387d
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-26 18:55:53 +0200

    Installed in BB.Messaging MassTransit.RabbitMq and created IServiceCollection extension to register the it with required configs

---
+++ src/Building Blocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+    {
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly is not null)
+                config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), config =>
+                {
+                    config.Username(configuration["MessageBroker:UserName"]!);
+                    config.Password(configuration["MessageBroker:Password"]!);
+                });
+
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
