bdbf85eca9f02c3da322338fcf135b4eb5461ca7
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-28 17:26:49 +0200

    Supported rate limiter for ordering service

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,10 +1,24 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("customPolicy", opt =>
+    {
+        opt.PermitLimit = 4;
+        opt.Window = TimeSpan.FromSeconds(10);
+    });
+});
+
+
 // configurations
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
