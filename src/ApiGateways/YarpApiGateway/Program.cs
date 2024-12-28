ba49e23b9501683e8c4dbcf47fa15284ecabed7c
yshakhshir <yazan.shakhshir@gmail.com>
2024-12-28 17:22:29 +0200

    Added reverse proxy routing configuration for catalog,basket, and ordering services

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 // configurations
 var app = builder.Build();
 
+app.MapReverseProxy();
 
 app.Run();
